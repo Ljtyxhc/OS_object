699dad86e7f431e81796b607869191efb0def3ed
Ljtyxhc_zwxqg <1839580965@qq.com>
2022-09-15 14:37:20 +0800

    测试 增加另外一个基础的窗口类（可以打印字符串）

--- Example.java
+++ Example.java
@@ -11,7 +11,8 @@ public class Example
     {
         EventQueue.invokeLater(() ->
         {
-            var frame = new SimpleFrame();
+            var frame = new NotHelloWorldFrame();
+            frame.setTitle("Not hello world");
             frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
             frame.setVisible(true);
         });
@@ -23,9 +24,36 @@ class SimpleFrame extends JFrame
     private static final int DEFAULT_WIDTH = 500;
     private static final int DEFAULT_HEIGHT = 600;
 
+
     public SimpleFrame()
     {
         setSize(DEFAULT_WIDTH, DEFAULT_HEIGHT);
+    }
+}
+
+class NotHelloWorldFrame extends JFrame
+{
+    public NotHelloWorldFrame()
+    {
+        add(new MyComponent());
+        pack();
+    }
+}
+class MyComponent extends JComponent
+{
+    private static final int DEFAULT_WIDTH = 500;
+    private static final int DEFAULT_HEIGHT = 600;
+
+    public static final int MESSAGE_X = 75;
+    public static final int MESSAGE_Y = 100;
+    public void paintComponent(Graphics g)
+    {
+        g.drawString("say hellow to this world",MESSAGE_X,MESSAGE_Y);
+    }
+
+    public Dimension getPreferredSize()
+    {
+        return new Dimension(DEFAULT_WIDTH,DEFAULT_HEIGHT);
     }
 }
 
